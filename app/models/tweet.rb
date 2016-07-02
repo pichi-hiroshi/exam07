afbbaa73b34516a8a0f0dac8b83b102754cba403
Hiroshi Takahashi <hrs.takahashi7@gmail.com>
2016-07-02 00:04:23 +0000

    デザイン調整

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
     
-    validates :content, presence: true
+    validates :content, length: { in: 1..255 }
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
         end
     end
     
+    root 'tweets#index'
 
 end
